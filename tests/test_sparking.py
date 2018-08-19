0de03248c3dfb20dc7c5c23b6c8e0d65eaef778d
tiantianaa <tiantianchai@caixin.com>
2018-08-19 15:25:33 +0800

    oooo

--- tests/test_sparking.py
+++ tests/test_sparking.py
@@ -16,10 +16,10 @@ class TestSparking(object):
 
 
     @pytest.mark.parametrize("bits_num, key, expected", [
-        (0, 0, 1),
-        (0, 0, 2),
-        (0, 0, 3),
-        (0, 0, 4)
+        (8, 5, 2),
+        (16, 5, 2),
+        (32, 5, 2),
+        (64, 5, 2)
     ])
 
     def testpoint(self, bits_num, key, expected):
