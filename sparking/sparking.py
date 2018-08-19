6432fc6fa5a4705e26836d701c4f00e6137d1bc3
tiantianaa <tiantianchai@caixin.com>
2018-08-19 20:10:01 +0800

    dhsfjir

--- sparking/sparking.py
+++ sparking/sparking.py
@@ -9,17 +9,10 @@ class Sparking:
             cnt += 1
             key &= key -1
         return bits_num - cnt
-    @staticmethod
-    def get_num_of_one(bits_num, key):
-        cnt = 0
-        while key:
-            cnt += 1
-            key &= key -1
-        return cnt
 
 def main():
     s = Sparking()
-    s.testcase(32, 5)
+    s.testcase(5, 5)
 
 if __name__ == '__main__':
     main()
--- tests/test_sparking.py
+++ tests/test_sparking.py
@@ -6,17 +6,7 @@ from sparking.sparking import Sparking
 
 class TestSparking(object):
     @pytest.mark.parametrize("bits_num, key, expected", [
-        (0, 0, 1),
-        (0, 0, 2),
-        (0, 0, 3),
-        (0, 0, 4)
-    ])
-    def test_get_num_of_one(self, bits_num, key, expected):
-        assert Sparking.get_num_of_one(bits_num, key) == expected
-
-    @pytest.mark.parametrize("bits_num, key, expected", [
         (8, 5, 6),
-        (32, 5, 30),
         (64, 5, 62)
     ])
     def testcase(self, bits_num, key, expected):
