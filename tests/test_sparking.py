553e430756023bbb93de274fdafa1adeb10b51e8
tiantianaa <tiantianchai@caixin.com>
2018-08-19 20:38:43 +0800

    ajdjiofa

--- tests/test_sparking.py
+++ tests/test_sparking.py
@@ -10,7 +10,17 @@ class TestSparking(object):
         (64, 5, 62)
     ])
     def testcase(self, bits_num, key, expected):
+
         assert Sparking.testcase(bits_num, key) == expected
+
+
+# class TestSparking(object):
+#     @pytest.mark.parametrize("bits_num, key, expected", [
+#         (8, 5, 6),
+#         (64, 5, 62)
+#     ])
+#     def testcase(self, bits_num, key, expected):
+#         assert Sparking.testcase(bits_num, key) == expected
 
 
 
