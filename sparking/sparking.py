4b3e535a1c4d3271b946af688c4d3ea01060c216
tiantianaa <tiantianchai@caixin.com>
2018-08-19 19:16:09 +0800

    baole

--- sparking/sparking.py
+++ sparking/sparking.py
@@ -9,13 +9,13 @@ class Sparking:
             cnt += 1
             key &= key -1
         return bits_num - cnt
-    # @staticmethod
-    # def get_num_of_one(bits_num, key):
-    #     cnt = 0
-    #     while key:
-    #         cnt += 1
-    #         key &= key -1
-    #     return cnt
+    @staticmethod
+    def get_num_of_one(bits_num, key):
+        cnt = 0
+        while key:
+            cnt += 1
+            key &= key -1
+        return cnt
 
 def main():
     s = Sparking()
--- tests/test_sparking.py
+++ tests/test_sparking.py
@@ -5,25 +5,23 @@ import pytest
 from sparking.sparking import Sparking
 
 class TestSparking(object):
-    # @pytest.mark.parametrize("bits_num, key, expected", [
-    #     (0, 0, 1),
-    #     (0, 0, 2),
-    #     (0, 0, 3),
-    #     (0, 0, 4)
-    # ])
-    # def test_get_num_of_one(self, bits_num, key, expected):
-    #     assert Sparking.get_num_of_one(bits_num, key) == expected
-
-
     @pytest.mark.parametrize("bits_num, key, expected", [
         (8, 5, 2),
         (16, 5, 2),
         (32, 5, 2),
         (64, 5, 2)
     ])
+    def test_get_num_of_one(self, bits_num, key, expected):
+        assert Sparking.get_num_of_one(bits_num, key) == expected
 
-    def testpoint(self, bits_num, key, expected):
-        assert Sparking.testpoint(bits_num, key) == expected
+    @pytest.mark.parametrize("bits_num, key, expected", [
+        (8, 5, 6),
+        (16, 5, 14),
+        (32, 5, 30),
+        (64, 5, 62)
+    ])
+    def test_get_num_of_zero(self, bits_num, key, expected):
+        assert Sparking.get_num_of_zero(bits_num, key) == expected
 
 
 
