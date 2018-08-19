c0ab48cb34756828c33f3ef8006d44a960d1cf48
tiantianaa <tiantianchai@caixin.com>
2018-08-19 15:20:37 +0800

    tttt

--- sparking/sparking.py
+++ sparking/sparking.py
@@ -3,23 +3,23 @@
 '''
 class Sparking:
     @staticmethod
-    def get_num_of_zero(bits_num, key):
+    def testpoint(bits_num, key):
         cnt = 0
         while key:
             cnt += 1
             key &= key -1
         return bits_num - cnt
-    @staticmethod
-    def get_num_of_one(bits_num, key):
-        cnt = 0
-        while key:
-            cnt += 1
-            key &= key -1
-        return cnt
+    # @staticmethod
+    # def get_num_of_one(bits_num, key):
+    #     cnt = 0
+    #     while key:
+    #         cnt += 1
+    #         key &= key -1
+    #     return cnt
 
 def main():
     s = Sparking()
-    s.get_num_of_zero(32, 5)
+    s.testpoint(32, 5)
 
 if __name__ == '__main__':
     main()
